8c5a9c02d2037bd75cf0431e2bdb57263c19fbe9
goliksim <golev.sanya2012@yandex.ru>
2024-05-13 18:50:43 +0300

    feat: added FlightSearchBloc

---
+++ lib/main_features/flight_tickets/end_search/bloc/flight_search_events.dart
@@ -0,0 +1,12 @@
+part of 'flight_search_bloc.dart';
+
+@freezed
+abstract class FlightSearchEvent with _$FlightSearchEvent {
+  const factory FlightSearchEvent.loadRoute(FlightRoute route) = _FlightSearchLoad;
+  const factory FlightSearchEvent.preSearch() = _FlightPreSearch;
+  const factory FlightSearchEvent.search() = _FlightSearch;
+
+  const factory FlightSearchEvent.updateRequestDate(DateTime date) = _FlightSearchDateChanger;
+  const factory FlightSearchEvent.updateReverseDate(DateTime? date) = _FlightSearchWayChanger;
+  const factory FlightSearchEvent.updateTicketTypes(int count, FlightTicketType type) = _FlightSearchTypesChanger;
+}
